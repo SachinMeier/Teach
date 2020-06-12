073a919fda9359c32b7fb43d9d8aa3d1ca62afea
Pillagr <Pillagr6@gmail.com>
2020-06-12 18:05:19 -0400

    add fibonacci

--- intro.py
+++ intro.py
@@ -30,7 +30,7 @@ list_bool = [True, False, True, True]
 # so first element is index 0, second = 1...last = size - 1
 list_int[0] = 3
 # list_int is now = [3,1,2,4,5]
-print(list_int[2]) # prints 2
+#print(list_int[2]) # prints 2
 
 
 # Dictionaries
@@ -45,50 +45,50 @@ dictionary = {
 # dictionary can be accessed by key, and returns value
 dictionary["apple"] = "green"
 
-print(dictionary["banana"]) # prints "yellow"
+#print(dictionary["banana"]) # prints "yellow"
 
+def ifFuncs():	
+	# if Statements follow this format:
+	# if ( STATEMENT ):
+	#  [TAB] COMMANDS
+	# 
+	# STATEMENT must evaluate to a boolean (true or false) value. 
+	# nonzero Numbers are always true
+	# strings are true if non-empty
+	# >, <, ==, <=, >=, are all comparison operattors that yield true or false
 
-# if Statements follow this format:
-# if ( STATEMENT ):
-#  [TAB] COMMANDS
-# 
-# STATEMENT must evaluate to a boolean (true or false) value. 
-# nonzero Numbers are always true
-# strings are true if non-empty
-# >, <, ==, <=, >=, are all comparison operattors that yield true or false
+	# Indents are absolutely necessary in python
+	# COMMANDS can be any number of lines of normal python code, including additional ifs, loops, 
+	# these commands will only be executed if the STATEMENT is true.
 
-# Indents are absolutely necessary in python
-# COMMANDS can be any number of lines of normal python code, including additional ifs, loops, 
-# these commands will only be executed if the STATEMENT is true.
+	if yes: # True always evaluates to true, so COMMANDS will execute
+		print("yes")
+	if no: # False always false, so COMMANDS doesnt execute
+		print("no") # never prints
+	if x == 5: # to evaluate if 2 objects are equal, use  ==. 
+		print("x == 5")
+	if x > 6:
+		print("x > 6")
 
-if yes: # True always evaluates to true, so COMMANDS will execute
-	print("yes")
-if no: # False always false, so COMMANDS doesnt execute
-	print("no") # never prints
-if x == 5: # to evaluate if 2 objects are equal, use  ==. 
-	print("x == 5")
-if x > 6:
-	print("x > 6")
+	# after an if, you can add an 'elif' statement, (else if), 
+	# which acts as an if statement only if the previous if statement
+	# is false. any number of elifs can be appended.
+	# lastly, an else statement can end a block of if/elifs. 
 
-# after an if, you can add an 'elif' statement, (else if), 
-# which acts as an if statement only if the previous if statement
-# is false. any number of elifs can be appended.
-# lastly, an else statement can end a block of if/elifs. 
+	# only 1 if
+	if x < 4: 
+		print("x < 4")
+	elif x > 4: #else if -> elif
+		print("x > 4")
+	elif x == 5:
+		print("x = 5")
+	# any number of elifs
+	#only one else
+	else:
+		print("x = 4")
 
-# only 1 if
-if x < 4: 
-	print("x < 4")
-elif x > 4: #else if -> elif
-	print("x > 4")
-elif x == 5:
-	print("x = 5")
-# any number of elifs
-#only one else
-else:
-	print("x = 4")
-
-if ((x == 5) or not yes) and not no:
-	print("at least one is true")
+	if ((x == 5) or not yes) and not no:
+		print("at least one is true")
 """
 LOOPS: 
 	loops are blocks of code that are executed repeatedly. Each time through the list of commands in
@@ -115,71 +115,112 @@ LOOPS:
 	- while (STATEMENT):
 		COMMANDS
 """
-for x in list_int:
-	print(x) # prints 3 then 1 then 2 4 5
+def loopFuncs():
+	for x in list_int:
+		print(x) # prints 3 then 1 then 2 4 5
 
-for i, x in enumerate(list_str):
-	print(i, ":", x) #prints "0: Hello", then "1: World", then "2: Goodbye"
+	for i, x in enumerate(list_str):
+		print(i, ":", x) #prints "0: Hello", then "1: World", then "2: Goodbye"
 
 
-while (yes):
-	print("s")
-	print("a")
-	print("d")
-	print("f")
-	print("g")
-	break
+	while (yes): # an infitite loop
+		print("s")
+		print("a")
+		print("d")
+		print("f")
+		print("g")
+		break # break ends the most immediate loop, so it only executes once
+		# if you comment this break statement out, it will print these letters infinitely
 
 
 
 """
-Recursive Function
+Recursive Function:
+recursive functions call themselves. To understand them, try copy-pasting 
+the entire function each time you see it call itself. this function will 
+increment a given number then call itself, passing that same number. After
+the called function is done, it will print y. This results in it printing 
+he numbers in reverse order, because the innermost iteration finishes first
+before execution returns to upper iterations. Illustration:
+
+r = the addition function (y +=1 )
+p = the print part of the funtion (print(y, end=' '))
+y = the var (starting at 0)
+
+
+r0 -> r1 -> r2 -> r3 -> p3(4) -> p2(3) -> p(2) -> p0(1)
+y=1	  y=2	y=3	  y=4  
 
 """
-def myfunc(y):
+def recursiveFunc(y):
 	y += 1
 	if y < 9:
-		myfunc(y)
-	print(y)
+		recursiveFunc(y)
+	print(y, end=' ')
+
+def fibonacci(limit=15):
+	a = 1
+	b = 1
+	for _ in range(limit):
+		print(b)
+		c = a 
+		a += b
+		b = c
+
+def recursiveFib(a, b, lim=15):
+	if lim:
+		c = a 
+		a += b
+		b = c
+		print(b)
+		recursiveFib(a, b, lim=lim-1)
+		
+
+#recursiveFunc(0) # prints 9 8 7 6 5 4 3 2 1 
+
+def stringFuncs():
+	""" 
+	Working with strings
+	"""
+	gender = False
+	greeting = "hello"
+	name = "Sachin"
+	m = "male"
+	f = "female"
+	age = 19
+	#Inefficient way (with error: Can't add int to string)
+	#message = greeting + " " + name + ". You are " + age + "years old."
+
+	'''
+	Better way: use f"words {variable or logic}"
+	alternative:
+	"words {}, {}".format(var1, var2)
+
+	'''
+	message = f"{greeting} {name}. You are {age*12} months old."
+	msg2 = "{} {}. You are {} months old".format(greeting, name, age*12)
+	print(message)
+
+	# STRING INDEXING
+	'''
+	Strings can be indexed like lists
+
+	string[x:y:z]
+	x = start point (inclusive)
+	y = end point (non-inclusive)j
+	z = interval (default =1, -1 = backwards)
+	any of the positions can be left blank
+	'''
+	greeting[0] # = "h"
+	greeting[:3] # = "hel"
+	greeting[1:3] # = "el" note: doesnt include the second l (index [3])
+	greeting[1:] # = "ello"
+	greeting[0:3:2] # = "hl" prints every other letter: 0,2
+	greeting[::-1] # = "olleh", start and end left blank
+	greeting[-1] # = "o" negative indeces start with -1 = last element, -2 = second to last...
+	greeting[1:-2] # = "el" negative indeces can be used with positive ones.
 
 
-""" 
-Working with strings
-"""
-gender = False
-greeting = "hello"
-name = "Sachin"
-m = "male"
-f = "female"
-age = 19
-#Inefficient way (with error: Can't add int to string)
-#message = greeting + " " + name + ". You are " + age + "years old."
-
-'''
-Better way: use f"words {variable or logic}"
-alternative:
-"words {}, {}".format(var1, var2)
-
-'''
-message = f"{greeting} {name}. You are {age*12} months old."
-msg2 = "{} {}. You are {} months old".format(greeting, name, age*12)
-print(message)
-
-# STRING INDEXING
-'''
-Strings can be indexed like lists
-
-string[x:y:z]
-x = start point (inclusive)
-y = end point (non-inclusive)
-z = interval (default =1, -1 = backwards)
-any of the positions can be left blank
-'''
-greeting[0] # = "h"
-greeting[:3] # = "hel"
-greeting[1:3] # = "el" note: doesnt include the second l (index [3])
-greeting[1:] # = "ello"
-greeting[0:3:2] # = "hl" prints every other letter: 0,2
-greeting[::-1] # = "olleh", start and end left blank
-greeting[-1] # = "o" negative indeces start with -1 = last element, -2 = second to last...
-greeting[1:-2] # = "el" negative indeces can be used with positive ones.
+if __name__ == "__main__":
+	#fibonacci(10)
+	recursiveFib(1,1,15)
